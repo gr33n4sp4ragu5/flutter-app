c1e0a1bf2554ae887d7b222f360ccd785f3732fb
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-05-09 22:01:32 +0200

    feat(#2): Save token and refresh token in shared preferences

---
+++ lib/util/shared_preference.dart
@@ -0,0 +1,35 @@
+import 'package:collective_intelligence_metre/domain/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+
+class UserPreferences {
+  Future<void> saveUser(User user) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("token", user.token);
+    prefs.setString("refreshToken", user.refreshToken);
+  }
+
+  Future<User> getUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    String token = prefs.getString("token");
+    String refreshToken = prefs.getString("refreshToken");
+
+    return User(
+        token: token,
+        refreshToken: refreshToken);
+  }
+
+  void removeUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    prefs.remove("token");
+    prefs.remove("refreshToken");
+  }
+
+  Future<String> getToken(args) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString("token");
+    return token;
+  }
+}
