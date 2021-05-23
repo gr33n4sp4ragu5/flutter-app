ef26087f6dc64199c51e9bec2234f60895bce5a6
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-05-23 15:53:28 +0200

    feat(#3): Send the survey result to my endpoint

--- lib/linear_survey_page.dart
+++ lib/linear_survey_page.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
+import 'package:collective_intelligence_metre/util/app_url.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:research_package/research_package.dart';
+import 'package:collective_intelligence_metre/util/shared_preference.dart';
 import 'research_package_objects/linear_survey_objects.dart';
 import 'dart:convert';
 
@@ -9,7 +14,7 @@ class LinearSurveyPage extends StatelessWidget {
   void resultCallback(RPTaskResult result) {
     // Do anything with the result
     String formattedResult = _encode(result);
-    print(formattedResult.substring(1018));
+    send_survey(formattedResult);
     print(_encode(result));
   }
 
@@ -18,7 +23,49 @@ class LinearSurveyPage extends StatelessWidget {
     print("The result so far:\n" + _encode(result));
   }
 
-  @override
+  Future<Map<String, dynamic>> send_survey(String formatted_result) async {
+
+    final Map<String, dynamic> survey_data = {
+      'survey': formatted_result
+    };
+
+    String token = await UserPreferences.getToken();
+
+    return await post(AppUrl.sendSurveyAnswer,
+        body: json.encode(survey_data),
+        headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token })
+        .then(onValue)
+        .catchError(onError);
+  }
+  static Future<FutureOr> onValue(Response response) async {
+    var result;
+    final Map<String, dynamic> responseData = json.decode(response.body);
+
+    print(response.statusCode);
+    if (response.statusCode == 201) {
+
+      result = {
+        'status': true,
+        'message': 'Survey answer successfully sent'
+      };
+    } else {
+      result = {
+        'status': false,
+        'message': 'Failed to send the answer of the survey',
+        'data': responseData
+      };
+    }
+
+    return result;
+  }
+
+  static onError(error) {
+    print("the error is $error.detail");
+    return {'status': false, 'message': 'Unsuccessful Request', 'data': error};
+  }
+
+
+@override
   Widget build(BuildContext context) {
     bool isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Theme(
