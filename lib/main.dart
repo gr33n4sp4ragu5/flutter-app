701fee4bae8b666b7054b7cf3db948b82a16d09a
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-05-27 21:45:57 +0200

    feat(#6): Added localization to surveys

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:collective_intelligence_metre/providers/auth.dart';
 import 'package:collective_intelligence_metre/providers/user_provider.dart';
 import 'package:collective_intelligence_metre/util/shared_preference.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:research_package/research_package.dart';
 
 import 'domain/user.dart';
 import 'pages/linear_survey_page.dart';
@@ -25,6 +27,35 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => UserProvider()),
       ],
       child: MaterialApp(
+          supportedLocales: [
+            Locale('en'),
+            Locale('es'),
+          ],
+          localizationsDelegates: [
+            // A class which loads the translations from JSON files
+            RPLocalizations.delegate,
+            // Built-in localization of basic text for Cupertino widgets
+            GlobalCupertinoLocalizations.delegate,
+            // Built-in localization of basic text for Material widgets
+            GlobalMaterialLocalizations.delegate,
+            // Built-in localization for text direction LTR/RTL
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          // Returns a locale which will be used by the app
+          localeResolutionCallback: (locale, supportedLocales) {
+            // Check if the current device locale is supported
+            for (var supportedLocale in supportedLocales) {
+              if (supportedLocale.languageCode == locale.languageCode
+              /*  && supportedLocale.countryCode == locale.countryCode */
+              // TODO: Test on physical iPhone if Locale should use countryCode instead
+              ) {
+                return supportedLocale;
+              }
+            }
+            // If the locale of the device is not supported, use the first one
+            // from the list (English, in this case).
+            return supportedLocales.first;
+          },
           title: 'Collective Intelligence Metre',
           theme: ThemeData(
             primarySwatch: Colors.blue,
