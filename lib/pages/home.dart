c009fbdbaa4fee1ba52e9511530f1a09a63c47b9
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-05-29 13:52:12 +0200

    feat(#4): Added bottom navigation bar first draft

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,36 +1,41 @@
 import 'package:collective_intelligence_metre/pages/linear_survey_page.dart';
+import 'package:collective_intelligence_metre/pages/login.dart';
+import 'package:collective_intelligence_metre/pages/register.dart';
 import 'package:collective_intelligence_metre/pages/tds.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+/// This is the stateful widget that the main application instantiates.
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static  List<Widget> _widgetOptions = <Widget>[
+    LinearSurveyPage(),
+    Login(),
+    Register()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ColIntMet'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-          child: Text('Comienza a responder una encuesta'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => LinearSurveyPage()),
-            );
-          },
-        ),
-          ElevatedButton(
-            child: Text('Comienza a responder una encuesta'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => TDSSurveyPage()),
-              );
-            },
-          ),
-        ]
-      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -45,15 +50,16 @@ class Home extends StatelessWidget {
             icon: Icon(Icons.watch),
             label: 'Actividad',
           ),
-      //    BottomNavigationBarItem(
-      //      icon: Icon(Icons.insert_chart),
-       //     label: 'Estadísticas',
-       //   ),
+          //    BottomNavigationBarItem(
+          //      icon: Icon(Icons.insert_chart),
+          //     label: 'Estadísticas',
+          //   ),
         ],
-        currentIndex: 1,
+        currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
-        //onTap: _onItemTapped,
+        onTap: _onItemTapped,
       ),
     );
   }
 }
+
