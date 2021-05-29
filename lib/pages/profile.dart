1445c069138ced5fb8c1518364de5e3e477078ba
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-05-29 14:19:58 +0200

    feat(#4): Added page for user profile which will show personal data and will let you edit it

---
+++ lib/pages/profile.dart
@@ -0,0 +1,18 @@
+import 'package:collective_intelligence_metre/pages/linear_survey_page.dart';
+import 'package:collective_intelligence_metre/pages/tds.dart';
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Column(
+          children: [
+            Text("Perfil de usuario")
+
+          ]
+      ),
+    );
+  }
+}
