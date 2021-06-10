97c0432d689c710a48f712f1513835ad2f26b67d
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-10 20:00:46 +0100

    fix(#19): serialize date to String before sending it in post request

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -73,7 +73,7 @@ class AuthProvider with ChangeNotifier {
         'password': password,
         'name': name,
         'surnames': surnames,
-        'birthdate': birthdate,
+        'birthdate': birthdate.toString(),
         'gender': gender.gender
     };
 
