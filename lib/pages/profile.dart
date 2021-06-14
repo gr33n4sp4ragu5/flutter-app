fe6ffe909be4b6ba975824586ce31ed57fc8a692
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-14 12:26:34 +0100

    feat(#17): Added AsyncMemoizer and TextEditingControllers
    I need to clean the code and assign the values to my properties

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -11,6 +11,7 @@ import 'package:http/http.dart';
 import 'package:provider/provider.dart';
 import 'package:collective_intelligence_metre/util/constants.dart';
 import 'package:collective_intelligence_metre/pages/register.dart';
+import 'package:async/async.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -25,17 +26,39 @@ class _ProfileState extends State<Profile> {
   Gender _gender;
   int _genderSelectedValue = 0;
   DateTime selectedDate;
+  final AsyncMemoizer _memoizer = AsyncMemoizer();
+  final emailController = new TextEditingController();
+  final nameController = new TextEditingController();
+  final surnamesController = new TextEditingController();
+  Future<dynamic> _profileData;
 
-  Future<Map<String, dynamic>> getProfileData() async {
+// Only fetch data once and set its text value
+// Setting it here ensures that the value doesn't go back
+// to the stored user value on every widget rebuild
 
-    String token = await UserPreferences.getToken();
-    print("The token is:");
-    print(token);
+  Future<dynamic> getProfileData() {
+    return this._memoizer.runOnce(() async {
+      String token = await UserPreferences.getToken();
+      print("The token is:");
+      print(token);
 
-    return await get(AppUrl.getProfileData,
-        headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token })
-        .then(assignValues)
-        .catchError(onError);
+      final profile_data = await get(AppUrl.getProfileData,
+          headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token })
+          .then(deserializeProfileData)
+          .catchError(onError);
+      print(profile_data.runtimeType);
+      emailController.text = profile_data["email"];
+      nameController.text = profile_data["name"];
+      surnamesController.text = profile_data["surnames"];
+
+      return profile_data;
+
+    });
+  }
+
+  deserializeProfileData(Response response) {
+    final Map<String, dynamic> responseData = json.decode(response.body)["profile_data"];
+    return responseData;
   }
 
   assignValues(Response response) {
@@ -122,34 +145,31 @@ class _ProfileState extends State<Profile> {
   @override
   initState() {
     super.initState();
-    getProfileData();
+    _profileData = getProfileData();
     selectedDate = DateTime(1990);
     _email = "pruebilla@gmail.com";
   }
-
-  @override
-  Widget build(BuildContext context) {
-    AuthProvider auth = Provider.of<AuthProvider>(context);
-
+  Widget populateForm(BuildContext context, AsyncSnapshot snapshot) {
     final emailField = TextFormField(
       autofocus: false,
       validator: validateEmail,
       onSaved: (value) => _email = value,
       decoration: buildInputDecoration(ENTER_EMAIL, Icons.email),
-      initialValue: _email,
+      controller: emailController,
     );
 
     final nameField = TextFormField(
       autofocus: false,
       validator: validateName,
       onSaved: (value) => _name = value,
+      controller: nameController,
     );
 
     final surnamesField = TextFormField(
       autofocus: false,
       validator: validateName,
       onSaved: (value) => _surnames = value,
-      initialValue: _surnames,
+      controller: surnamesController,
     );
 
     final birthdateField = InputDatePickerFormField(
@@ -214,6 +234,142 @@ class _ProfileState extends State<Profile> {
     );
 
 
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(40.0),
+          child: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 15.0),
+                    label("Email"),
+                    SizedBox(height: 5.0),
+                    emailField,
+                    SizedBox(height: 15.0),
+                    label("Fecha de nacimiento"),
+                    SizedBox(height: 10.0),
+                    birthdateField,
+                    SizedBox(height: 15.0),
+                    label("Nombre"),
+                    SizedBox(height: 10.0),
+                    nameField,
+                    SizedBox(height: 15.0),
+                    label("Apellidos"),
+                    SizedBox(height: 10.0),
+                    surnamesField,
+                    SizedBox(height: 10.0),
+                    label("Seleccione su sexo:"),
+                    SizedBox(height: 10.0),
+                    maleRadioButton,
+                    SizedBox(height: 10.0),
+                    femaleRadioButton,
+                    SizedBox(height: 20.0),
+                    otherRadioButton,
+                    longButtons("Guardar cambios", modifyProfileData),
+                    // auth.loggedInStatus == Status.Authenticating
+                    //   ? loading
+                    // : longButtons("Guardar cambios", modifyProfileData),
+                  ],
+                ),
+              )
+
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //AuthProvider auth = Provider.of<AuthProvider>(context);
+/*
+    final emailField = TextFormField(
+      autofocus: false,
+      validator: validateEmail,
+      onSaved: (value) => _email = value,
+      decoration: buildInputDecoration(ENTER_EMAIL, Icons.email),
+      controller: emailController,
+    );
+
+    final nameField = TextFormField(
+      autofocus: false,
+      validator: validateName,
+      onSaved: (value) => _name = value,
+      controller: nameController,
+    );
+
+    final surnamesField = TextFormField(
+      autofocus: false,
+      validator: validateName,
+      onSaved: (value) => _surnames = value,
+      controller: surnamesController,
+    );
+
+    final birthdateField = InputDatePickerFormField(
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2005),
+      initialDate: selectedDate,
+      onDateSubmitted: (date) {
+        setState(() {
+          selectedDate = date;
+        });
+      },
+    );
+
+    final male = Radio(
+        value: 1,
+        groupValue: _genderSelectedValue,
+        onChanged: (T) {
+          _gender = new Gender('male');
+          setState(() {
+            print(T);
+            _genderSelectedValue = T;
+          });
+        }
+    );
+    final female = Radio(
+        value: 2,
+        groupValue: _genderSelectedValue,
+        onChanged: (T) {
+          _gender = new Gender('female');
+          setState(() {
+            print(T);
+            _genderSelectedValue = T;
+          });
+        }
+    );
+
+    final other = Radio(
+        value: 3,
+        groupValue: _genderSelectedValue,
+        onChanged: (T) {
+          _gender = new Gender('other');
+          setState(() {
+            print(T);
+            _genderSelectedValue = T;
+          });
+        }
+    );
+
+    final maleRadioButton = ListTile(
+        title: const Text('Hombre'),
+        leading: male
+    );
+
+    final femaleRadioButton = ListTile(
+        title: const Text('Mujer'),
+        leading: female
+    );
+
+    final otherRadioButton = ListTile(
+        title: const Text('Prefiero no contestar'),
+        leading: other
+    );
+
+/*
     var loading = Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
@@ -222,8 +378,16 @@ class _ProfileState extends State<Profile> {
       ],
     );
 
-    
+ */
 
+ */
+
+    return FutureBuilder(
+      future: _profileData,
+      builder: populateForm,
+    );
+
+/*
     return SafeArea(
       child: Scaffold(
         body: Container(
@@ -270,6 +434,8 @@ class _ProfileState extends State<Profile> {
         ),
       ),
     );
+
+    */
   }
 }
 
