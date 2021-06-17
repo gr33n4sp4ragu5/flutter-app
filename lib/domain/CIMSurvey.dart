f9def837d842648cd6e29074129ab976c0f34e7a
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-17 20:12:00 +0100

    feat(#21): Added the toWidget in the model

--- lib/domain/CIMSurvey.dart
+++ lib/domain/CIMSurvey.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 enum SurveyState {
@@ -12,4 +13,19 @@ class CIMSurvey {
   String title;
 
   CIMSurvey(this.surveyPage, this.surveyId, this.state, this.nextQuestionStep, this.title);
+
+  Widget toWidget(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(this.title),
+        tileColor: Color(0xFF36ABC4),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => this.surveyPage),
+          );
+        },
+      ),
+    );
+  }
 }
