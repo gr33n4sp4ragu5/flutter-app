17b4ed747a045aa16df61d8847f26673f5f8be5b
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-17 20:17:45 +0100

    feat(#21): Made preloaded surveys a list of CIMSurveys and created a constructor which initialize the list of preloaded surveys

--- lib/surveys/surveys_preloaded.dart
+++ lib/surveys/surveys_preloaded.dart
@@ -4,10 +4,14 @@ import '../pages/linear_survey_page.dart';
 import '../pages/tds.dart';
 
 class PreloadedSurveys {
-  List preloaded_surveys = [];
+  List<CIMSurvey> preloaded_surveys = [];
+
+  PreloadedSurveys() {
+    preloadSurveys();
+  }
 
   void preloadSurveys() {
-    List surveys = [];
+    List<CIMSurvey>  surveys = [];
     CIMSurvey read_mind_eyes = new CIMSurvey(LinearSurveyPage(), "ReadMindEyes", SurveyState.NEW, "ninguno", "Reading the mind in the eyes");
     CIMSurvey team_diagnostic_survey = new CIMSurvey(TDSSurveyPage(), "TeamDiagnosticSurvey", SurveyState.NEW, "ninguno", "Team Diagnostic Survey");
     surveys.add(read_mind_eyes);
