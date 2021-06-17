47502006284a795234a2f34ce93349c9584bcbad
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-17 20:16:14 +0100

    feat(#21): turn Surveys into a stateful Widget with the list of preloaded surveys

--- lib/pages/surveys.dart
+++ lib/pages/surveys.dart
@@ -1,42 +1,24 @@
-import 'package:collective_intelligence_metre/pages/linear_survey_page.dart';
-import 'package:collective_intelligence_metre/pages/tds.dart';
+import 'package:collective_intelligence_metre/domain/CIMSurvey.dart';
+import 'package:collective_intelligence_metre/surveys/surveys_preloaded.dart';
 import 'package:flutter/material.dart';
 
-class Surveys extends StatelessWidget {
+class Surveys extends StatefulWidget {
+  PreloadedSurveys preloadedSurveys = new PreloadedSurveys();
+  @override
+  _SurveysState createState() => _SurveysState();
+}
+
+class _SurveysState extends State<Surveys> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-          children: [
-            Text(
-              "Encuestas disponibles",
-              style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.5),
-            ),
-            Card(
-              child: ListTile(
-                title: Text('Reading the mind in the eyes'),
-                tileColor: Color(0xFF36ABC4),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LinearSurveyPage()),
-                  );
-                },
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text('Team Diagnostic Survey'),
-                tileColor: Color(0xFF36ABC4),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TDSSurveyPage()),
-                  );
-                },
-              ),
-            )
+          children: formatSurveys(context));
+  }
 
-          ]
-      );
+  List<Widget> formatSurveys(BuildContext context) {
+    List<CIMSurvey> surveys = widget.preloadedSurveys.preloaded_surveys;
+    List<Widget> formattedSurveys = [];
+    surveys.forEach((element) {formattedSurveys.add(element.toWidget(context));});
+    return formattedSurveys;
   }
 }
