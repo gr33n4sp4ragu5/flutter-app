121c79ea14a2430029cf579631dab1d036ec0201
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-18 12:42:29 +0100

    feat(#21): added first draft of the call to backend to get the surveys finished by the user

--- lib/surveys/surveys_preloaded.dart
+++ lib/surveys/surveys_preloaded.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 import 'package:collective_intelligence_metre/domain/CIMSurvey.dart';
+import 'package:collective_intelligence_metre/util/app_url.dart';
+import 'package:collective_intelligence_metre/util/errors.dart';
+import 'package:collective_intelligence_metre/util/shared_preference.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 import '../pages/linear_survey_page.dart';
 import '../pages/tds.dart';
@@ -10,12 +17,35 @@ class PreloadedSurveys {
     preloadSurveys();
   }
 
-  void preloadSurveys() {
+  void preloadSurveys() async {
     List<CIMSurvey>  surveys = [];
-    CIMSurvey read_mind_eyes = new CIMSurvey(LinearSurveyPage(), "ReadMindEyes", SurveyState.NEW, "ninguno", "Reading the mind in the eyes");
-    CIMSurvey team_diagnostic_survey = new CIMSurvey(TDSSurveyPage(), "TeamDiagnosticSurvey", SurveyState.NEW, "ninguno", "Team Diagnostic Survey");
+    SurveyState read_mid_eyes_survey_state = await getCurrentSurveyState("ReadMindEyes");
+    SurveyState tds_state = await getCurrentSurveyState("TeamDiagnosticSurvey");
+    CIMSurvey read_mind_eyes = new CIMSurvey(LinearSurveyPage(), "ReadMindEyes", read_mid_eyes_survey_state, "ninguno", "Reading the mind in the eyes");
+    CIMSurvey team_diagnostic_survey = new CIMSurvey(TDSSurveyPage(), "TeamDiagnosticSurvey", tds_state, "ninguno", "Team Diagnostic Survey");
     surveys.add(read_mind_eyes);
     surveys.add(team_diagnostic_survey);
     preloaded_surveys = surveys;
+  }
+
+  Future<SurveyState> getCurrentSurveyState(String surveyId) async {
+    String token = await UserPreferences.getToken();
+    print("The token is:");
+    print(token);
+
+    final finished_surveys = await get(AppUrl.getProfileData,
+        headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token })
+        .then(deserializeFInishedSurveysResponse)
+        .catchError(onError);
+    if (finished_surveys.contains(surveyId)) {
+      return SurveyState.FINISHED;
+    } else{
+      return SurveyState.NEW;
+    }
+  }
+
+  List deserializeFInishedSurveysResponse(Response response) {
+    final List finished_surveys = json.decode(response.body)["finished_surveys"];
+    return finished_surveys;
   }
 }
