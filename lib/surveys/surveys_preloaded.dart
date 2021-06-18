39e6924fb08cc7f3f81a794abe7064a171fe2580
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-18 13:33:55 +0100

    feat(#21): Removed constructor and initialize it in another method. Added logging and put the right survey ids which should me constants in constants file. Still need to add future builder so that it shows a spinner and then show already the loaded surveys

--- lib/surveys/surveys_preloaded.dart
+++ lib/surveys/surveys_preloaded.dart
@@ -12,17 +12,20 @@ import '../pages/tds.dart';
 
 class PreloadedSurveys {
   List<CIMSurvey> preloaded_surveys = [];
-
+/*
   PreloadedSurveys() {
     preloadSurveys();
   }
 
+ */
+
   void preloadSurveys() async {
     List<CIMSurvey>  surveys = [];
-    SurveyState read_mid_eyes_survey_state = await getCurrentSurveyState("ReadMindEyes");
-    SurveyState tds_state = await getCurrentSurveyState("TeamDiagnosticSurvey");
-    CIMSurvey read_mind_eyes = new CIMSurvey(LinearSurveyPage(), "ReadMindEyes", read_mid_eyes_survey_state, "ninguno", "Reading the mind in the eyes");
-    CIMSurvey team_diagnostic_survey = new CIMSurvey(TDSSurveyPage(), "TeamDiagnosticSurvey", tds_state, "ninguno", "Team Diagnostic Survey");
+    SurveyState read_mid_eyes_survey_state = await getCurrentSurveyState("surveyTaskID");
+    SurveyState tds_state = await getCurrentSurveyState("surveyTDS");
+    CIMSurvey read_mind_eyes = new CIMSurvey(LinearSurveyPage(), "surveyTaskID", read_mid_eyes_survey_state, "ninguno", "Reading the mind in the eyes");
+    CIMSurvey team_diagnostic_survey = new CIMSurvey(TDSSurveyPage(), "surveyTDS"
+        "", tds_state, "ninguno", "Team Diagnostic Survey");
     surveys.add(read_mind_eyes);
     surveys.add(team_diagnostic_survey);
     preloaded_surveys = surveys;
@@ -33,7 +36,7 @@ class PreloadedSurveys {
     print("The token is:");
     print(token);
 
-    final finished_surveys = await get(AppUrl.getProfileData,
+    final finished_surveys = await get(AppUrl.getFinishedSurveys,
         headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token })
         .then(deserializeFInishedSurveysResponse)
         .catchError(onError);
@@ -46,6 +49,8 @@ class PreloadedSurveys {
 
   List deserializeFInishedSurveysResponse(Response response) {
     final List finished_surveys = json.decode(response.body)["finished_surveys"];
+    print("finished_surveys");
+    print(finished_surveys);
     return finished_surveys;
   }
 }
