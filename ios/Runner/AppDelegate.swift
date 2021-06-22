e16523bcc3733362242246ffd818828152e9b368
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-22 10:42:49 +0200

    conf(#15): Added configuration in iOS for flutter_local_contifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,11 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Code added for flutter_local_notifications package set up
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
