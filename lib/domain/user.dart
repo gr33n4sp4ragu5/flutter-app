e0e104e0cc4bf3b134e871eea3b87fba300b4e71
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-25 19:33:14 +0200

    feat(#24): Added expiracy for token and refresh token in shared preferences

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -2,13 +2,17 @@ class User {
 
   String token;
   String refreshToken;
+  DateTime tokenExpiration;
+  DateTime refreshTokenExpiration;
 
-  User({this.token, this.refreshToken});
+  User({this.token, this.refreshToken, this.tokenExpiration, this.refreshTokenExpiration});
 
   factory User.fromJson(Map<String, dynamic> responseData) {
     return User(
         token: responseData['access_token'],
-        refreshToken: responseData['refresh_token']
+        refreshToken: responseData['refresh_token'],
+        tokenExpiration: DateTime.now().add(Duration(seconds: responseData["token_expires_in"])),
+        refreshTokenExpiration: DateTime.now().add(Duration(seconds: responseData["refresh_expires_in"]))
     );
   }
 }
--- lib/util/shared_preference.dart
+++ lib/util/shared_preference.dart
@@ -7,17 +7,32 @@ class UserPreferences {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString("token", user.token);
     prefs.setString("refreshToken", user.refreshToken);
+    prefs.setString("tokenExpiration", user.tokenExpiration.toIso8601String());
+    prefs.setString("refreshTokenExpiration", user.refreshTokenExpiration.toIso8601String());
   }
 
   Future<User> getUser() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-
+    DateTime tokenExpiration = getTokenExpiration(prefs, 'tokenExpiration');
+    DateTime refreshTokenExpiration = getTokenExpiration(prefs, 'refreshTokenExpiration');
     String token = prefs.getString("token");
     String refreshToken = prefs.getString("refreshToken");
 
     return User(
         token: token,
-        refreshToken: refreshToken);
+        refreshToken: refreshToken,
+        tokenExpiration: tokenExpiration,
+        refreshTokenExpiration: refreshTokenExpiration
+    );
+  }
+
+  DateTime getTokenExpiration(SharedPreferences prefs, String prefsProperty) {
+    String tokenExpirationPref = prefs.get(prefsProperty);
+    if(tokenExpirationPref == null) {
+      return null;
+    } else {
+      return DateTime.parse(tokenExpirationPref);
+    }
   }
 
   void removeUser() async {
