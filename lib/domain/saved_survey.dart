f481372e77c0ea2667c355a487b6c432c50c97e3
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-26 22:06:10 +0200

    feat(#16): Changed the data type of rawResults and added a constructor for SavedSurveys

--- lib/domain/saved_survey.dart
+++ lib/domain/saved_survey.dart
@@ -2,10 +2,13 @@ import 'package:research_package/model.dart';
 
 class SavedSurvey{
 
-List<RPTaskResult> rawResults;// The results as they are returned from the survey package when finalized
+RPTaskResult rawResults;// The results as they are returned from the survey package when finalized
 String lastStepAnsweredId; // The id of the last step which was answered
 String userEmail; // The user whose survey we are saving, as multiple accounts can be used.
 String surveyId; // Which survey we are saving.
+
+SavedSurvey(this.rawResults, this.lastStepAnsweredId, this.userEmail, this.surveyId);
+
 //TODO- expirationDate when we will delete this survey (Not a priority)
   SavedSurvey.FromJson(Map<String, dynamic> json)
       : rawResults = json['rawResults'],
