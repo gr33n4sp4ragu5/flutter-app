c3d64ad8f57e22be50cba08e0c8063d490ba8baa
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-06-29 19:47:14 +0200

    feat(#16): made read me in the eyes survey a stateless widget

--- lib/pages/linear_survey_page.dart
+++ lib/pages/linear_survey_page.dart
@@ -3,17 +3,12 @@ import 'package:collective_intelligence_metre/util/save_survey.dart';
 import 'package:flutter/material.dart';
 import '../research_package_objects/linear_survey_objects.dart';
 
-class LinearSurveyPage extends StatefulWidget {
+class LinearSurveyPage extends StatelessWidget{
 
   @override
-  _LinearSurveyPageState createState() => _LinearSurveyPageState();
-}
-
-class _LinearSurveyPageState extends State<LinearSurveyPage> {
-
-@override
   Widget build(BuildContext context) {
 
     return FutureBuilder(future: getTaskAsync(linearSurveyTask), builder: buildSurvey);
   }
 }
+
