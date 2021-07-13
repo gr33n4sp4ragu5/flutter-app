1cca47b70f5265bc421d866ba7ed31c105919aff
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-07-13 10:04:43 +0200

    feat(#37): Changed baseUrl to point to the proxy using HTTPS

--- lib/util/app_url.dart
+++ lib/util/app_url.dart
@@ -1,5 +1,5 @@
 class AppUrl {
-  static const String liveBaseURL = "http://138.4.110.213:8000";
+  static const String liveBaseURL = "https://ci.syst.etsisi.upm.es/api/v1/colintmet";
   static const String localBaseURL = "http://192.168.56.101:8000";
 
   static const String baseURL = liveBaseURL;
