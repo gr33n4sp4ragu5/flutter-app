88633977c0c77451dc1b5748416f31512e28a90b
Alberto Hurtado <a.hurtado@alumnos.upm.es>
2021-07-13 13:33:22 +0200

    feat(#41): Only trust untrusted certificates if it is from our server

--- lib/util/resolving_ssl.dart
+++ lib/util/resolving_ssl.dart
@@ -4,6 +4,9 @@ class MyHttpOverrides extends HttpOverrides{
   @override
   HttpClient createHttpClient(SecurityContext context){
     return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) {
+        final isValidHost = host == "ci.syst.etsisi.upm.es";
+        return isValidHost;
+      };
   }
 }
